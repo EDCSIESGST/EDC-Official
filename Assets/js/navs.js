47a0eb710a03036aacfde36394dd378879075480
Ved Gawade <vedgawade1211@gmail.com>
2023-03-12 21:16:55 +0530

    final commit

---
+++ Assets/js/navs.js
@@ -0,0 +1,12 @@
+function runMyFunction() {
+    $( document ).ready(function() {
+   
+        $('.dropdown-item').click(function(){
+         $('.navbar div').removeClass('show');
+      });  
+      $('.single').click(function(){
+             $('.navbar div').removeClass('show');
+      });
+     
+       });   
+  }
